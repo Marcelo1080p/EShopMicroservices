0e56a0e570a35c8246f049b470e5b75a9e9c47a0
unknown <juniooor.2015@gmail.com>
2025-06-05 19:55:43 -0300

    adding Product microservice

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,18 +1,35 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
 
 namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand
     (
         string Name, List<string> Categories, string Description, string ImageFile, decimal Price
-    ) : IRequest<CreateProductResult>;
+    ) : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
-internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler 
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
         // Simulate product creation logic
-        throw new NotImplementedException("Product creation logic is not implemented yet.");
+
+        //save to database
+
+        //return result createproductresult result
+
+        var product = new Product
+        { 
+            Name = command.Name,
+            Categories = command.Categories,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+
+        };
+
+         return new CreateProductResult(Guid.NewGuid());
     }
 
 }
